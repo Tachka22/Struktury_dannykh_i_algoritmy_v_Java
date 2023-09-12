55d00d36a888e6c8b522d711a965ae4716ba8812
Vlad <chinnov.vlad@gmail.com>
2023-09-12 23:21:34 +0300

    реализовал класс highArray

---
+++ array/src/highArray.java
@@ -0,0 +1,11 @@
+public class highArray {
+    private long[] arr;//Ссылка на массив arr
+    private int elements;//Элементы в массиве
+
+    public highArray(int max){
+        this.arr =new long[max];
+        elements = 0;
+    }
+
+
+}
