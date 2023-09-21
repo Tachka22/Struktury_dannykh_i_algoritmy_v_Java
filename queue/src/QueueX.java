d5b9f1bb6f84a0ff5aff18403644ee3b7c1806d4
Vlad <chinnov.vlad@gmail.com>
2023-09-22 00:05:01 +0300

    реализация очереди на основе массива

--- queue/src/QueueX.java
+++ queue/src/QueueX.java
@@ -1,2 +1,45 @@
 public class QueueX {
+    private int maxSize;
+    private long[] queueArray;
+    private int front;
+    private int rear;
+    private int nElems;
+
+    public QueueX(int s)//Конструктор.
+    {
+        maxSize = s;
+        queueArray = new long[maxSize];
+        front = 0;
+        rear = -1;
+        nElems = 0;
+    }
+
+    public void insert(long value)
+    {
+        if (rear == maxSize - 1)
+            rear = -1;
+        queueArray[++rear] = value;
+        nElems++;
+    }
+    public long remove()
+    {
+        long temp = queueArray[front++];
+        if (front == maxSize)
+            front = 0;
+        nElems--;
+        return temp;
+    }
+    public long peekFront() // Чтение элемента
+    {
+        return queueArray[front];
+    }
+    public boolean isEmpty() // true, если очередь пуста
+    {
+        return (nElems==0);
+    }
+    public boolean isFull() // true, если очередь заполнена
+    {
+        return (nElems == maxSize);
+    }
+
 }
