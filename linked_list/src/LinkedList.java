cb9d72ab3c06dddf11929f0e2a1ed3f4a2c9c7ba
Vlad <chinnov.vlad@gmail.com>
2023-09-25 00:08:54 +0300

    реализация связного списка

---
+++ linked_list/src/LinkedList.java
@@ -0,0 +1,72 @@
+public class LinkedList<T> {
+    private static class Node<T>{
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        private T data;
+        private Node<T> next;
+
+        public Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+    private Node<T> head;
+
+    public LinkedList(){
+        head = null;
+    }
+
+    public LinkedList(T[] data){
+        for (T item : data) add(item);
+    }
+
+    public void add(T data){
+        Node<T> newNode = new Node<T>(data);
+        if (head == null) head = newNode;
+        else{
+            Node<T> curNode = head;
+            while (curNode.next != null){
+                curNode  = curNode.next;
+            }
+            curNode.next = newNode;
+        }
+
+    }
+
+    public void Reverse(){
+        Node<T> previousNode = null;
+        Node<T> currentNode = head;
+        Node<T> nextNode = null;
+        while (currentNode != null){
+            nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        head = previousNode;
+    }
+
+    public void Print(){
+        Node<T> temp = head;
+        while (temp != null){
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+}
