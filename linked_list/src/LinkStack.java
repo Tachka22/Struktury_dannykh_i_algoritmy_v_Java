f8611ce140625fabb64fc41a6fcb3f98cb6ce835
Vlad <chinnov.vlad@gmail.com>
2023-09-26 19:44:18 +0300

    реализовал стек на базе связного списка

---
+++ linked_list/src/LinkStack.java
@@ -0,0 +1,16 @@
+public class LinkStack<T>{
+    private LinkedList theList;
+    public LinkStack(){
+        theList = new LinkedList<>();
+    }
+    public void push(T value){
+        theList.prepend(value);
+    }
+    public T pop(){
+        return (T) theList.deleteFirst();
+    }
+    public boolean isEmpty(){
+        return theList.isEmpty();
+    }
+
+}
